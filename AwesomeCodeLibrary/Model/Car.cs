ff8fa85341836a7b5ca4d56207409fc1abdf6312
Richard Ebbers <richard@ebbersonline.nl>
2017-04-28 12:14:28 +0200

    Intitial commit of a project to unit test

---
+++ AwesomeCodeLibrary/Model/Car.cs
@@ -0,0 +1,11 @@
+﻿namespace AwesomeCodeLibrary.Model
+{
+    public class Car : Vehicle
+    {
+        public Car(int topSpeed) : base (topSpeed)
+        {
+            SpeedLimit = 130;
+            BrakeSpeedInMeterPerSecond = 12;
+        }
+    }
+}
